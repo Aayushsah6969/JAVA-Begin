f3db0153296d163003ce1ea464567c41849f3db1
aayushsah6969 <aayushsah269@gmail.com>
2024-12-13 22:52:24 +0530

    printn --> println

--- OOPSJ-Lab/Lab-1/username.java
+++ OOPSJ-Lab/Lab-1/username.java
@@ -1,8 +1,8 @@
 import java.util.*;
 class username{
 public static void main(String args[]){
-Scanner in =new Scanner(system.in);
-System.out.printn("Enter your first name");
+Scanner in =new Scanner(System.in);
+System.out.println("Enter your first name");
 String fa =in.nextLine();
 System.out.println("Enter you last name");
 String la = in.nextLine();
