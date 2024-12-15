c853e1612190c7e03486f4562e51eae67cd97aed
aayushsah6969 <aayushsah269@gmail.com>
2024-12-15 16:11:27 +0530

    for each

--- Java-Practice/array2.java
+++ Java-Practice/array2.java
@@ -1,15 +1,20 @@
 import java.util.*;
+
 public class array2 {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner nums = new Scanner(System.in);
         int arr[] = new int[5];
-        for(int i=0; i<arr.length; i++){
-            System.out.println("Enter "+i+"th element: ");
-            arr[i]= nums.nextInt();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Enter " + i + "th element: ");
+            arr[i] = nums.nextInt();
         }
         System.out.println("Your array is: ");
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i]+" ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+        String cars[]={"Volvo","BMW","Ford","Mazda"};
+        for (String i : cars) {
+            System.out.println(i);
         }
     }
 }
