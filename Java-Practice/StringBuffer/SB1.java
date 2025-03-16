58b9e5338dd20a5f140f3499dee51a1df1fc2ee5
aayushsah6969 <aayushsah269@gmail.com>
2025-03-16 15:13:32 +0530

    wrote stringbuffer

--- Java-Practice/StringBuffer/SB1.java
+++ Java-Practice/StringBuffer/SB1.java
@@ -2,11 +2,11 @@
 StringBuffer Class append() Method
 The append() method concatenates the given argument with this String.
 */
-class SB1{  
-public static void main(String args[]){  
-StringBuffer sb=new StringBuffer("Hello "); 
-System.out.println(sb);//prints Hello  
-sb.append("Java");//now original string is changed  
-System.out.println(sb);//prints Hello Java  
-}  
-}  
+class SB1 {
+    public static void main(String args[]) {
+        StringBuffer sb = new StringBuffer("Hello ");
+        System.out.println(sb);// prints Hello
+        sb.append("Java");// now original string is changed
+        System.out.println(sb);// prints Hello Java
+    }
+}
--- Java-Practice/StringBuffer/SB4.java
+++ Java-Practice/StringBuffer/SB4.java
@@ -1,10 +1,10 @@
 /*StringBuffer delete() Method
 The delete() method of the StringBuffer class deletes the String from the specified beginIndex to endIndex.
 */
-class SB4{  
-public static void main(String args[]){  
-StringBuffer sb=new StringBuffer("Hello");  
-sb.delete(0,6);  
-System.out.println(sb);//prints Hlo  
-}  
-}  
+class SB4 {
+    public static void main(String args[]) {
+        StringBuffer sb = new StringBuffer("Hello");
+        sb.delete(0, 6);
+        System.out.println(sb);// prints Hlo
+    }
+}
